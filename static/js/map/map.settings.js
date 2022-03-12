1a4540391b6adccce4cbc8578653289e762782bc
Michael Krug <michi.krug@gmail.com>
2022-03-12 19:28:02 +0100

    Add ui parts

--- static/js/map/map.settings.js
+++ static/js/map/map.settings.js
@@ -361,6 +361,68 @@ function initSettingsSidebar() {
             updatePokemons()
             Store.set('scaleByValues', this.checked)
         })
+
+        $('#pokemon-highlight-switch').on('change', function () {
+            settings.highlightPokemon = this.checked
+            const highlightPokemonWrapper = $('#highlight-pokemon-wrapper')
+            if (this.checked) {
+                highlightPokemonWrapper.show()
+            } else {
+                highlightPokemonWrapper.hide()
+            }
+            updatePokemons()
+            Store.set('highlightPokemon', this.checked)
+        })
+
+        var highlightIvSlider = document.getElementById('highlight-iv-slider')
+        noUiSlider.create(highlightIvSlider, {
+            start: [0, settings.highlightThresholdIV],
+            connect: 'lower',
+            step: 1,
+            range: {
+                min: 0,
+                max: 100
+            },
+            format: {
+                to: function (value) {
+                    return Math.round(value)
+                },
+                from: function (value) {
+                    return Number(value)
+                }
+            }
+        })
+        highlightIvSlider.noUiSlider.on('change', function () {
+            settings.highlightThresholdIV = this.get()
+            $('#highlight-iv-slider-title').text(`${i18n('min. IVs')} (${settings.highlightThresholdIV}%)`)
+            updatePokemons()
+            Store.set('highlightThresholdIV', settings.highlightThresholdIV)
+        })
+
+        var highlightLevelSlider = document.getElementById('highlight-level-slider')
+        noUiSlider.create(highlightLevelSlider, {
+            start: [0, settings.highlightThresholdLevel],
+            connect: 'lower',
+            step: 1,
+            range: {
+                min: 0,
+                max: 35
+            },
+            format: {
+                to: function (value) {
+                    return Math.round(value)
+                },
+                from: function (value) {
+                    return Number(value)
+                }
+            }
+        })
+        highlightLevelSlider.noUiSlider.on('change', function () {
+            settings.highlightThresholdLevel = this.get()
+            $('#highlight-level-slider-title').text(`${i18n('min. Level')} (L${settings.highlightThresholdLevel})`)
+            updatePokemons()
+            Store.set('highlightThresholdLevel', settings.highlightThresholdLevel)
+        })
     }
 
     if (serverSettings.rarity) {
